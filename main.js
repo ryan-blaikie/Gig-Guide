6807b4dc4b8b1fbb3d929b97712a5b3ccd420e82
Ryan Blaikie <ryan.blaikie@gmail.com>
2020-09-15 14:03:49 +1200

    Install node-fetch and retrieve data from API

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
 const express = require('express');
 const app = express();
-app.listen(3000, ()=> console.log('listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json());
+
+const fetch = require('node-fetch');
+
+//API access info
+const username = "findagig";
+const pw = "m74tgzy6hyxb";
+let data; 
+
+
+app.listen(3000, ()=> {
+    console.log('listening at 3000');
+    fetch("https://api.eventfinda.co.nz/v2/events.json", {
+    method: 'GET',    
+    headers: {'Authorization': 'Basic ' + Buffer.from(`${username}:${pw}`).toString('base64')},
+
+    })
+    .then (response => response.json())
+    .then(data => {
+        console.log("Retrieved data from EventFinda API");
+        data = data.events;
+    })
+    .catch(err => console.log(err));
+})
+
+//Let client JS access data from EventFinda API
+// app.get('/getData', (req, res) => {
+//     res.json({
+//         body: data
+//     })
+// })
---
+++ public/client.js
@@ -0,0 +1,15 @@
+async function getEventData(){
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type':'application/json'
+        }
+    }
+
+    const res = await fetch('/getData', options);
+    let data = await res.json();
+    console.log(data);
+}
+
+// getEventData();
